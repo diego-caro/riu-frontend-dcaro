9530f7e048315679637c802f580630073d888214
Diego Caro <diego.caro999@gmail.com>
2026-08-25 12:26:03 -0300

    add operation count to skip overlap. [HI-7]

--- frontend/src/app/services/loading.service.ts
+++ frontend/src/app/services/loading.service.ts
@@ -6,11 +6,17 @@ export class LoadingService {
   private readonly loadingSubject = new BehaviorSubject<boolean>(false);
   readonly loading$ = this.loadingSubject.asObservable();
 
+  private count = 0;
+
   show(): void {
+    this.count++;
     this.loadingSubject.next(true);
   }
 
   hide(): void {
-    this.loadingSubject.next(false);
+    this.count = Math.max(0, this.count - 1);
+    if (this.count === 0) {
+      this.loadingSubject.next(false);
+    }
   }
 }
